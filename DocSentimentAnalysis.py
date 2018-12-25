02ec22e4a6d7c468c697cf4524c48407be7e138a
jepyh <41173467+jepyh@users.noreply.github.com>
2018-12-25 11:49:26 +0800

    Add files via upload

---
+++ DocSentimentAnalysis.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: DocSentimentAnalysis.py
+# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
+# Date: 18-7-16
+# 修改：jepyh https://github.com/jepyh/DocDicSA
+
+from sentence_parser import *
+import ahocorasick
+import jieba
+import re
+import math
+import os
+
+class Sentimentor():
+    def __init__(self):
+        CUR_DIR = os.getcwd()#'/'.join(os.path.abspath(".//").split('/')[:-1])
+        DICT_DIR = os.path.join(CUR_DIR,'dict')
+        DescPath = os.path.join(DICT_DIR, 'desc_words.txt')
+        SenPath = os.path.join(DICT_DIR, 'sentiment_words.txt')
+        self.DescDict = {i.strip().split('\t')[0]:float(i.strip().split('\t')[1]) for i in open(DescPath, encoding='utf-8') if i.strip()}
+        self.SenDict = {i.strip().split('\t')[0]:float(i.strip().split('\t')[1]) for i in open(SenPath, encoding='utf-8') if i.strip()}
+        #print(self.DescDict)
+        #print(self.SenDict)
+        self.SenTree = self.build_actree(list(self.SenDict.keys()))
+        self.UserWords = list(set(list(self.DescDict.keys()) + list(self.SenDict.keys())))
+        jieba.load_userdict(self.UserWords)
+        self.senti_parser = LtpParser()
+
+    def build_actree(self, wordlist):
+        actree = ahocorasick.Automaton()
+        for index, word in enumerate(wordlist):
+            word = ' ' + word + ' '
+            actree.add_word(word, (index, word))
+        actree.make_automaton()
+        return actree
+
+    '''content分句处理'''
+    def seg_sentences(self, content):
+        return [sentence.replace('\u3000','').replace('\xc2\xa0', '').replace(' ','') for sentence in re.split(r'[!?？！:;。\n\r]',content) if sentence]
+
+    '''利用情感词过滤情感句'''
+    def check_senti(self, sentence):
+        flag = 0
+        word_list = list(jieba.cut(sentence))
+        # print(word_list)
+        senti_words = []
+        for i in self.SenTree.iter(' '.join(word_list + [' '])):
+            senti_words.append(i[1][1].replace(' ', ''))
+            flag += 1
+        return flag, word_list, senti_words
+
+    '''情感句过滤'''
+    def filter_sentence(self, sentences):
+        senti_sentences = list()
+        for index, sentence in enumerate(sentences):
+            flag, word_list, senti_words = self.check_senti(sentence)
+            if flag:
+                senti_sentences.append([index, sentence, word_list, senti_words])
+        return senti_sentences
+
+    '''sentence analysis'''
+    def get_sentence_score(self, sent_words, senti_words):
+        sent_postag = self.senti_parser.get_postag(sent_words)
+        sent_tuples = self.senti_parser.syntax_parser(sent_words, sent_postag)
+        dep_dict = self.senti_parser.parser_dict(sent_words, sent_postag, sent_tuples)
+        sent_score = 0.0
+        for dep in dep_dict:
+            word = dep[0]
+            word_desc = dep[3]
+            word_score = self.get_abs_sentiment(word, word_desc, senti_words)
+            sent_score += word_score
+            # print(dep, word_score)
+
+        return math.tanh(sent_score)
+
+    def get_abs_sentiment(self, word, word_desc, senti_words):
+        if word not in senti_words:
+            return 0.0
+        else:
+            word_score = self.SenDict.get(word, 0.6)
+            if not word_desc:
+                return word_score
+            else:
+                desc_words = []
+                for rel, info in word_desc.items():
+                    desc_words += [i[1] for i in info if i[2][0] not in ['w', 'u']]
+                for desc_word in desc_words:
+                    desc_score = self.DescDict.get(desc_word, 1.0)
+                    word_score *= desc_score
+            return word_score
+
+    def doc_sentiment_score(self, content):
+        sents = self.seg_sentences(content)
+        senti_sentences = self.filter_sentence(sents)
+        scores = []
+        for sent in senti_sentences:
+            sent_words = sent[2]
+            senti_words = sent[3]
+            sent_score = self.get_sentence_score(sent_words, senti_words)
+            # print(sent[1], sent_score)
+            if sent_score:
+                scores.append(sent_score)
+        if len(scores) > 0:
+            return sum(scores)/len(scores)
+        else:
+            return 0.0
+
+
---
+++ sentence_parser.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: sentence_parser.py
+# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
+# Date: 18-3-10
+# 修改：jepyh https://github.com/jepyh/DocDicSA
+
+import jieba
+from pyltp import Postagger, Parser
+import os
+
+class LtpParser():
+    def __init__(self):
+        CUR_DIR = os.getcwd() # '/'.join(os.path.abspath(__file__).split('/')[:-1])
+        LTP_DIR = os.path.join(CUR_DIR, "ltp_data")
+        self.postagger = Postagger()
+        self.postagger.load(os.path.join(LTP_DIR, "pos.model"))
+        self.parser = Parser()
+        self.parser.load(os.path.join(LTP_DIR, "parser.model"))
+        print(os.path.join(LTP_DIR, "pos.model"))
+        print(os.path.join(LTP_DIR, "parser.model"))
+
+    '''ltp基本操作'''
+    def get_postag(self, words):
+        return list(self.postagger.postag(words))
+
+    '''依存关系格式化'''
+    def syntax_parser(self, words, postags):
+        arcs = self.parser.parse(words, postags)
+        words = ['Root'] + words
+        postags = ['w'] + postags
+        dep_tuples = list()
+        for index in range(len(words)-1):
+            arc_index = arcs[index].head
+            arc_relation = arcs[index].relation
+            dep_tuples.append([index+1, words[index+1], postags[index+1], words[arc_index], postags[arc_index], arc_index, arc_relation])
+        return dep_tuples
+
+    '''为句子中的每个词语维护一个保存句法依存儿子节点的字典'''
+    def parser_dict_old(self, words, postags, tuples):
+        child_dict_list = list()
+        for index, word in enumerate(words):
+            child_dict = dict()
+            for arc in tuples:
+                if arc[3] == word:
+                    if arc[-1] in child_dict:
+                        child_dict[arc[-1]].append(arc)
+                    else:
+                        child_dict[arc[-1]] = []
+                        child_dict[arc[-1]].append(arc)
+            child_dict_list.append([word, postags[index], index, child_dict])
+        return child_dict_list
+
+
+    '''为句子中的每个词语维护一个保存句法依存儿子节点的字典'''
+    def parser_dict(self, words, postags, tuples):
+        child_dict_list = list()
+        for index, word in enumerate(words):
+            child_dict = dict()
+            for arc in tuples:
+                if arc[3] == word:
+                    rel = arc[-1]
+                    if rel in child_dict:
+                        child_dict[rel].append(arc)
+                    else:
+                        child_dict[rel] = []
+                        child_dict[rel].append(arc)
+                if arc[1] == word:
+                    rel = 'B_' + arc[-1]
+                    arc = [arc[-2], arc[-4], arc[-3], arc[1], arc[2], arc[0], rel]
+                    if rel in child_dict:
+                        child_dict[rel].append(arc)
+                    else:
+                        child_dict[rel] = []
+                        child_dict[rel].append(arc)
+            child_dict_list.append([word, postags[index], index, child_dict])
+        return child_dict_list
+
